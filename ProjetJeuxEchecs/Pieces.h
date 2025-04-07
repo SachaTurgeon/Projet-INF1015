9819541e37273275d028b93a2bf6a4d8ef2d29eb
SachaTurgeon <sacha.turgeon@polymtl.ca>
2025-04-07 12:55:14 -0400

    test2

--- ProjetJeuxEchecs/Pieces.h
+++ ProjetJeuxEchecs/Pieces.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <utility>
+#include <vector>
+
+class Piece
+{
+public:
+	Piece() = default;
+	Piece(std::pair<int, int> position, bool isWhite) : position_(position), isWhite_(isWhite) {};
+	virtual std::vector<std::pair<int, int>> calculateMoves() {}
+private:
+	std::pair<int, int> position_;
+	bool isWhite_;
+};
