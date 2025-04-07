34b3cbac9fc3f4dd0613499ded037207f68b06ea
SachaTurgeon <sacha.turgeon@polymtl.ca>
2025-04-07 14:24:09 -0400

    pawn class added

--- ProjetJeuxEchecs/Pieces.cpp
+++ ProjetJeuxEchecs/Pieces.cpp
@@ -6,3 +6,10 @@
 Piece::Piece(std::pair<int, int> position, bool isWhite, QWidget* parent) : QWidget(parent), position_(position), isWhite_(isWhite) {
 	this->setFixedSize(50, 50);
 };
+
+Pawn::Pawn(std::pair<int, int> position, bool isWhite, QWidget* parent) : Piece(position, isWhite, parent) {}
+
+std::vector<std::pair<int, int>> Pawn::calculateMoves() {
+	std::vector<std::pair<int, int>> moves;
+	return moves;
+}
--- ProjetJeuxEchecs/Pieces.h
+++ ProjetJeuxEchecs/Pieces.h
@@ -13,3 +13,12 @@ private:
 	std::pair<int, int> position_;
 	bool isWhite_;
 };
+
+class Pawn : public Piece
+{
+public:
+	Pawn(std::pair<int, int> position, bool isWhite, QWidget* parent = nullptr);
+	std::vector<std::pair<int, int>> calculateMoves() override;
+private:
+	bool hasMoved_ = false;
+};
